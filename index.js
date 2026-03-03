3b6e88da960aece4c8995d5467f20f62d102192e
sarah friday silas <sarahsilasfriday112@gmail.com>
2026-03-03 10:52:18 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.green("node and npm are working perfectly!"));
+console.log(chalk.blue("package installed successfully."));
+console.log(chalk.red("you are ready to build!"));
